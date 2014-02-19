71a310df3e7b9a735e4ebde7b775760042ace555
andersonk17474 <andersonk17474@gmail.com>
2014-02-19 12:09:04 -0500

    Update popup.js
    classified the popup and removed some legacy code

--- popup.js
+++ popup.js
@@ -1,191 +1,233 @@
 /*
-    --------------------------------------------------------------------------
-    Code for link-hover text boxes
-    By Nicolas Honing
-	Updated on 5-14-2013 by andersonk17474
-    Usage: <a onmouseover="popup('popup content','elemnt id','element class',width)">a link</a>
-     (width is optional - default is in CSS: #pup {width: x;},
-	 (element id is optional  - will default to 'pup')
-	 (class is optional - no default)
-      escape " in content with &quot;)
-    Tutorial and support at http://nicolashoening.de?twocents&nr=8
-    --------------------------------------------------------------------------
+--------------------------------------------------------------------------
+Code for link-hover text boxes
+By Nicolas Honing
+Updated on 2-19-2014 by kevin.anderson
+usage:
+ honingpop = new HoningPopup();
+ $('div.popup').on("mouseover", function(e){ honingpop.show({'msg':"test", 'bind':this, "class": "custompopup"}); });
+
+--------------------------------------------------------------------------
 */
-popupFunct = {};
-popupVars = {};
-popupVars.minMargin = 15; // set how much minimal space there should be (in pixels)
+var HoningPopup = function(){
+
+	if (!(this instanceof HoningPopup)) {
+		throw new TypeError("HoningPopup constructor cannot be called as a function.");
+	}
+
+	this.minMargin = 15; // set how much minimal space there should be (in pixels)
 									// between the popup and everything else (borders, mouse)
-popupVars.ready = false;  			// we are ready when the mouse event is set up
-popupVars.default_width = 200; // will be set to width from css in document.ready
-popupVars.defaultIdTag = "pup"; // default tag to use for target element
-popupVars.idTag;
-popupVars.currentMouseXY = [];  // holder for the current mouse position
-popupVars.timer;
+	this.ready = false;  			// we are ready when the mouse event is set up
+	this.default_width = 450; // will be set to popup width in document.ready
+	this.defaultIdTag = "pup"; // default tag to use for  popup element
+	this.idTag = null;
+	this.currentMouseXY = [];  // holder for the current mouse position
+	this.timer = null;
 
-// class used for styling a single popup differently 
-popupVars.defaultClass = ""; // default class to use for target element (none)
-popupVars.elemClass;
-
-
-// activate or deactivate the move hoverover
-popupVars.setReady = function(p_bool){
-	if (typeof p_bool === "boolean"){ popupVars.ready = p_bool; }
-}
-
-popupFunct.initNew = function(p_elemTag, p_elemClass){
-	var l_elemClass = "";
-	if (typeof p_elemClass === "string"){ l_class = l_elemClass; }
-	// only create the hidden target div if it doesnt already exist
-	if ( (typeof p_elemTag === "string") && (!($('#'+p_elemTag).length))){
-		$('body').append('<div id="'+p_elemTag+'" class="'+l_elemClass+'" style="position:abolute; display:none; z-index:200;"></div>');
-		css_width = $('#'+p_elemTag).width();
-		if (css_width != 0) popupVars.default_width = css_width;
-	}
-}
-
-
-/* Prepare popup and define the mouseover callback */
-jQuery(document).ready(function(){
-    popupVars.idTag = popupVars.defaultIdTag;  // init the popup tag to the default
-	popupVars.elemClass = popupVars.defaultClass; // init the popup class to the default
-	// create default popup on the page
-	// popupFunct.initNew(popupVars.idTag, popupVars.elemClass); // unnessesary - element should be created the first time the popup function called
+	// class used for styling a single popup differently 
+	this.defaultClass = ""; // default class to use for popup element (none)
+	this.elemClass = null;
 	
-    // set dynamic coords when the mouse moves
-    $(document).mousemove(function(e){ 
-		popupVars.currentMouseXY = popupFunct.readMousePos(e);
-		if (popupVars.ready){
-			popupFunct.setElementPos(popupVars.currentMouseXY);
+	this.init();
+
+};
+
+HoningPopup.prototype = {
+	
+	constructor: HoningPopup,
+
+	// activate or deactivate the move hoverover
+	setReady : function(p_bool){
+		if (typeof p_bool === "boolean"){ this.ready = p_bool; }
+	},
+	
+	/**
+	  add a new popup to the DOM if one does not already exist
+	  @method createNew
+	  @param {string} p_elemTag target element id for the new popup
+	  @param {string} p_elemClass optional class for the new popup
+	*/
+	createNew : function(p_elemTag, p_elemClass){
+		var l_elemClass = "", css_width;
+		if (typeof p_elemClass === "string"){ l_elemClass = p_elemClass; }
+		// only create the hidden target div if it doesnt already exist
+		if ( (typeof p_elemTag === "string") && (!($('#'+p_elemTag).length))){
+			$('body').append('<div id="'+p_elemTag+'" class="'+l_elemClass+'" style="position:absolute; display:none; z-index:200;"></div>');
+			css_width = $('#'+p_elemTag).width();
+			if (css_width !== 0) this.default_width = css_width;
 		}
-    });
-});
+	},
 
-// read the current mouse position when mouse movement even occurs
-popupFunct.readMousePos = function(e){
-	var x,y;
-    x = $(document).scrollLeft() + e.pageX;
-    y = $(document).scrollTop() + e.pageY;
-    x += 10; // important: if the popup is where the mouse is, the hoverOver/hoverOut events flicker
-	return ([x,y]);
-}
 
-// set the target element position
-popupFunct.setElementPos = function(p_xyArr,p_targetElemId){
-	var l_targetElemId = popupVars.idTag;
-	if (typeof p_targetElemId === "string"){ l_targetElemId = p_targetElemId; }
-	// only adjust the target element if it exists and the control flag is true
-	var l_targetElem = $('#'+l_targetElemId);
-	if (l_targetElem.length){
-		var x_y = popupFunct.nudge(p_xyArr[0],p_xyArr[1]); // avoids edge overflow
-		// remember: the popup is still hidden
-		l_targetElem.css('top', x_y[1] + 'px');
-		l_targetElem.css('left', x_y[0] + 'px');
-	}
+	/* Prepare popup and define the mouseover callback */
+	/**
+	  load the nessesary dom mouse listener events when the class is instanciated
+	  @method init 
+	  @link http://stackoverflow.com/questions/5706757/is-there-a-way-to-check-document-ready-if-jquery-is-not-available
+	*/
+	init : function(){
+	
+		var that = this, tid = setInterval( function () {
+			if ( document.readyState !== 'complete' ) return;
+			clearInterval( tid );       
+			// document loaded, begin setup
+			that.idTag = that.defaultIdTag;  // init the popup tag to the default
+			that.elemClass = that.defaultClass; // init the popup class to the default
+			// create default popup on the page
+			// this.createNew(this.idTag, this.elemClass); // unnessesary - element should be created the first time the popup function called
+			
+			// set dynamic coords when the mouse moves
+			$(document).mousemove(function(e){ 
+				that.currentMouseXY = that.readMousePos(e);
+				if (that.ready){
+					that.setElementPos(that.currentMouseXY);
+				}
+			});
+			
+		}, 100 );
+	
+	},
+	/**
+	  read the current mouse position when mouse movement even occurs
+	  and return it an array with x at 0 and y at 1
+	  @method: readMousePos
+	  @param {object} e javascript event object
+	  @returns {array}
+	*/
+	readMousePos : function(e){
+		var x,y;
+		// adding the scroll causes mislocation of the taget element issues...not sure why this was ever done
+		//x = $(document).scrollLeft() + e.pageX;  
+		//y = $(document).scrollTop() + e.pageY;
+		x = e.pageX;
+		y = e.pageY;
+		x += 10; // important: if the popup is where the mouse is, the hoverOver/hoverOut events flicker
+		return ([x,y]);
+	},
 
-}
-
-// delayed popup message
-// currently not working correctly - the first time it's displayed on the far left of screen no matter where the target element is
-// if your drag accross the target element just right (hard to describe) the onmouseout event to hide the target elementdoes not get fired
-popupFunct.delayedPopup = function(delay, p_msg, p_elemTag, p_elemClass , p_width){
-	// if the timer is active - clear it before starting
-	clearTimeout(popupVars.timer);
-	function callback(p_arr){
-		return function(){
-			popupFunct.popup(p_arr[0], p_arr[1], p_arr[2] , p_arr[3], p_arr[4] )
-		}
-	}
-	var l_msg = '"'+p_msg+'"';
-	var l_width = p_width;
-	if (typeof p_width === "undefined" || p_width == null){
-	   l_width = popupVars.default_width;
-	}
-	popupVars.timer = setTimeout(callback([l_msg, p_elemTag, p_elemClass, l_width, (arguments.callee.caller.arguments[0])]), delay);
-}
-
-/*
- The actual callback:
- Write message, show popup w/ custom width if necessary,
- make sure it disappears on mouseout
-*/
-popupFunct.popup = function(p_msg, p_elemTag, p_elemClass , p_width, p_callerFX )
-{
-	var l_elemClass = "";
-	if (typeof p_elemClass === "string"){ l_elemClass = p_elemClass; }
-	if (typeof p_elemTag !== "undefined" && p_elemTag != null && p_elemTag.length){
-		popupVars.idTag = p_elemTag; // reassign default object tag value
-		// create new popup on the page
-		popupFunct.initNew(popupVars.idTag, l_elemClass);
-	}
-	else{ popupVars.idTag = popupVars.defaultIdTag; }
-	var popupElement = $('#'+popupVars.idTag);
-	if (popupElement.length){
-		if (!(popupElement.hasClass(l_elemClass))){
-			popupElement.attr("class", "");  // clear the current class
-			// assign the new class
-			popupElement.addClass(l_elemClass);
-		}
-		popupVars.setReady(true);
-		// use default width if not customized here
-		if (typeof width === "undefined"){
-		   p_width = popupVars.default_width;
+	
+	/**
+	  set the target element position
+	  @method setElementPos
+	  @param {array} array of mouse co-ordinates [x,y]
+	  @param {string} p_targetElemId optional html elment id for setting the screen position
+	*/
+	setElementPos : function(p_xyArr,p_targetElemId){
+		var l_targetElemId = this.idTag;
+		if (typeof p_targetElemId === "string"){ l_targetElemId = p_targetElemId; }
+		// only adjust the target element if it exists and the control flag is true
+		var l_targetElem = $('#'+l_targetElemId);
+		if (l_targetElem.length){
+			var x_y = this.nudge(p_xyArr[0],p_xyArr[1]); // avoids edge overflow
+							
+			// remember: the popup is still hidden, add 5px to the y position
+			l_targetElem.css('top', (x_y[1]+5) + 'px');
+			l_targetElem.css('left', x_y[0] + 'px');
 		}
 
+	},
 
-		// write content and display
-		popupElement.html(p_msg).width(p_width).show();
-		// make sure popup goes away on mouse out
-		// the event obj needs to be gotten from the virtual 
-		//  caller, since we use onmouseover='popup(p_msg)' 
+	/**
+	 display the popup, and hide on mouseout from the target element
+	 @method show
+	 @param {object} p_obj
+	 @member {string} p_obj.msg html/text content to add to the popup
+	 @member {string} p_obj.id optional target element id
+	 @member {string} p_obj.class optional class for the popup
+	 @member {string} p_obj.width optional width for the popup
+	 @member {event}  p_obj.bind reference to the element that the show event is bound to
+	 @example  honingpop = new HoningPopup();
+	 $('div.popup').on("mouseover", function(e){ honingpop.show({'msg':"test", 'bind':this}); });
+	 */
+	
+	show : function( p_obj)
+	{
+		var that = this;
+		if (typeof p_obj !== "object"){
+			throw TypeError ("HoningPopup.show requires parameter to be of type \"Object\"");
+		}
+		if ( (!('bind' in p_obj)) || !p_obj.bind){
+			throw ReferenceError ("HoningPopup.show requires a reference to the bound element is passed as a parameter");
+		}
+	
+		var l_elemClass, l_width;
+		l_elemClass = p_obj.class || "";
+		l_width = p_obj.width || this.default_width;
+		this.idTag =  p_obj.id || this.defaultIdTag;
 		
-		var l_callerFX;
-		if (typeof p_callerFX === "undefined" || p_callerFX == null ) l_callerFX = arguments.callee.caller.arguments[0];
-		else l_callerFX = p_callerFX;
-		var t = popupFunct.getTarget(l_callerFX);
-		$(t).unbind('mouseout').bind('mouseout', 
-			 function(e){
-				 popupElement.hide().width(popupVars.default_width);
-			 }
-		);
-	}   
-}
+		// create new popup on the page
+		this.createNew(this.idTag, l_elemClass);
+	
+		
+		var popupElement = $('#'+this.idTag);
+		if (popupElement.length){
+			popupElement.attr("class", "");  // clear the current class(es)
+			popupElement.attr("class", l_elemClass);  // assign the class argument
+			this.setReady(true);
+			// write content and display
+			popupElement.html(p_obj.msg).width(l_width).show();
+			// make sure popup goes away on mouse out
+			$(p_obj.bind).one("mouseout", function(){
+				popupElement.hide().width(that.default_width);
+			})
 
-/* Avoid edge overflow */
-popupFunct.nudge = function(x,y)
-{
-    var win = $(window);
-    
-    // When the mouse is too far on the right, put window to the left
-    var xtreme = $(document).scrollLeft() + win.width() - $('#'+popupVars.idTag).width() - popupVars.minMargin;
-    if(x > xtreme) {
-        x -= $('#'+popupVars.idTag).width() + 2 * popupVars.minMargin;
-    }
-    x = popupFunct.max(x, 0);
+		}   
+	},
 
-    // When the mouse is too far down, move window up
-    if((y + $('#'+popupVars.idTag).height()) > (win.height() +  $(document).scrollTop())) {
-        y -= $('#'+popupVars.idTag).height() + popupVars.minMargin;
-    }
+	/*  */
+	/**
+	  Avoid target dom element from edge overflow
+	  returns array with updated co-ordinates [x,y]
+	  @method nudge
+	  @param {number} x target element x position
+	  @param {number} y target element y position 
+	  @returns {array}
+	*/
+	nudge : function(x,y)
+	{
+		var win = $(window);
+		
+		// When the mouse is too far on the right, put window to the left
+		var xtreme = $(document).scrollLeft() + win.width() - $('#'+this.idTag).width() - this.minMargin;
+		if(x > xtreme) {
+			x -= $('#'+this.idTag).width() + 2 * this.minMargin;
+		}
+		x = this.max(x, 0);
 
-    return [ x, y ];
-}
+		// When the mouse is too far down, move window up
+		if((y + $('#'+this.idTag).height()) > (win.height() +  $(document).scrollTop())) {
+			y -= $('#'+this.idTag).height() + this.minMargin;
+		}
 
-/* custom max */
-popupFunct.max = function(a,b){
-    if (a>b) return a;
-    else return b;
-}
+		return [ x, y ];
+	},
 
-/*
- Get the target (element) of an event.
- Inspired by quirksmode
-*/
-popupFunct.getTarget = function(e) {
-    var targ;
-    if (!e) var e = window.event;
-    if (e.target) targ = e.target;
-    else if (e.srcElement) targ = e.srcElement;
-    if (targ.nodeType == 3) // defeat Safari bug
-        targ = targ.parentNode;
-    return targ;
-}
+	/* custom max */
+	max : function(a,b){
+		if (a>b) return a;
+		else return b;
+	},
+
+	/*
+	 Get the target (element) of an event.
+	 Inspired by quirksmode
+	*/
+	getTarget : function(e) {
+		var targ;
+		if (!e){
+			e = window.event;
+		}
+		if (e.target){
+			targ = e.target;
+		}
+		else if (e.srcElement) targ = e.srcElement;
+		if (targ.nodeType == 3) // defeat Safari bug
+			targ = targ.parentNode;
+		return targ;
+	}
+};
+
+
+
+
+
