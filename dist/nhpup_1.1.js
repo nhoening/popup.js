f609284aa2b0b7be15a2baac699219872b312e45
nhoening <nhoening@gmail.com>
2014-03-26 21:10:37 +0100

    add MIT license

--- dist/nhpup_1.1.js
+++ dist/nhpup_1.1.js
@@ -1,18 +1,41 @@
 /*
-    --------------------------------------------------------------------------
-    Code for link-hover text boxes
-    By Nicolas Honing
-    Usage: <a onmouseover="nhpup.popup('popup text' [, {'class': 'myclass', 'width': 300}])">a link</a>
-    The configuration dict with CSS class and width is optional - default is class .pup and width of 200px.
-    You can style the popup box via CSS, targeting its ID #pup. 
-    You can escape " in the popup text with &quot;.
-    Tutorial and support at http://nicolashoening.de?twocents&nr=8
-    --------------------------------------------------------------------------
+
+--------------------------------------------------------------------------
+Code for link-hover text boxes
+By Nicolas Höning
+Usage: <a onmouseover="nhpup.popup('popup text' [, {'class': 'myclass', 'width': 300}])">a link</a>
+The configuration dict with CSS class and width is optional - default is class .pup and width of 200px.
+You can style the popup box via CSS, targeting its ID #pup. 
+You can escape " in the popup text with &quot;.
+Tutorial and support at http://nicolashoening.de?twocents&nr=8
+--------------------------------------------------------------------------
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Nicolas Höning
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 */
 
 nhpup = {
 
-    pup: null,      // This is out popup box, represented by a div    
+    pup: null,      // This is the popup box, represented by a div    
     identifier: "pup",  // Name of ID and class of the popup box
     minMargin: 15,  // Set how much minimal space there should be (in pixels)
                     // between the popup and everything else (borders, mouse)
